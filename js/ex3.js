c1f624d0d4b4f22b1b426caf7df286652ff76ac0
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-02-18 10:06:30 +0000

    Git ignore update

--- js/ex3.js
+++
@@ -1,8 +0,0 @@
-function validateName() {
-    let name = document.querySelector("#name").value;
-    let patt = new RegExp("[A-Za-z]+");
-    let res = patt.test(name);
-    if (!res){
-        alert("Only letters are allowed in names");
-    }
-}
